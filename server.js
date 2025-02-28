6a003016e7bfec0008c554cfc1d877c59b9b2aed
Doug-Silva <silvatec.br@gmail.com>
2025-02-27 22:48:03 -0300

    Conexão com MongoDB

--- server.js
+++ server.js
@@ -2,14 +2,23 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+//conexão com mongodb
+mongoose.connect('mongodb://localhost:27017/form-manut', {
+})
+  .then((res) => {
+    console.log("MongoDB CONECTADO com sucesso!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 app.use(cors());
-
 app.use(cookieParser());
-
 app.use(express.json());
 
 app.get('/', function (req, res) {
